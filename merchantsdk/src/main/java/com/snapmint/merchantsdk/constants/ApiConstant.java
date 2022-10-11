221117b91628517faf1477e87279e3a4ddbf9625
rakesh0021 <rakesh.talsaniya@softices.com>
2022-10-11 12:03:39 +0530

    initial project

---
+++ merchantsdk/src/main/java/com/snapmint/merchantsdk/constants/ApiConstant.java
@@ -0,0 +1,14 @@
+package com.snapmint.merchantsdk.constants;
+
+
+import com.snapmint.merchantsdk.utils.SnapmintResponse;
+
+public class ApiConstant {
+
+    //public static final String BASE_URL = "https://qa2.snapmint.com/api/loan_applications";
+    public static final String BASE_URL = "https://qaapi.snapmint.com/v1/public/online_checkout";
+    public static final String DATA = "data";
+    public static final String MERCHANT_CALLBACK = "callback";
+
+    public static SnapmintResponse snapmintPaymentresponse = null;
+}
---
+++ merchantsdk/src/main/java/com/snapmint/merchantsdk/models/EmiModel.java
@@ -0,0 +1,193 @@
+package com.snapmint.merchantsdk.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class EmiModel{
+
+	@SerializedName("pay_now_image1_pop_up_disable")
+	private String payNowImage1PopUpDisable;
+
+	@SerializedName("pay_now_text3_pop_up_disable")
+	private String payNowText3PopUpDisable;
+
+	@SerializedName("pay_now_text1_part1")
+	private String payNowText1Part1;
+
+	@SerializedName("pay_now_text1_part2")
+	private String payNowText1Part2;
+
+	@SerializedName("emi_second_percentage")
+	private String emiSecondPercentage;
+
+	@SerializedName("pay_now_text1_part3")
+	private String payNowText1Part3;
+
+	@SerializedName("pay_now_text1_part4")
+	private String payNowText1Part4;
+
+	@SerializedName("emi_one_percentage")
+	private String emiOnePercentage;
+
+	@SerializedName("pay_now_text1_pop_up_disable")
+	private String payNowText1PopUpDisable;
+
+	@SerializedName("pay_now_percentage")
+	private String payNowPercentage;
+
+	@SerializedName("pay_now_text2_pop_up_disable")
+	private String payNowText2PopUpDisable;
+
+	@SerializedName("emi_rates_percentage_pop_up_disable")
+	private String emiRatesPercentagePopUpDisable;
+
+	@SerializedName("pay_now_text4_pop_up_disable")
+	private String payNowText4PopUpDisable;
+
+	@SerializedName("pay_now_text3")
+	private String payNowText3;
+
+	@SerializedName("pay_now_image_pop_up_disable")
+	private String payNowImagePopUpDisable;
+
+	@SerializedName("pay_now_percentage_pop_up_disable")
+	private String payNowPercentagePopUpDisable;
+
+	@SerializedName("pay_now_text2")
+	private String payNowText2;
+
+	public void setPayNowImage1PopUpDisable(String payNowImage1PopUpDisable){
+		this.payNowImage1PopUpDisable = payNowImage1PopUpDisable;
+	}
+
+	public String getPayNowImage1PopUpDisable(){
+		return payNowImage1PopUpDisable;
+	}
+
+	public void setPayNowText3PopUpDisable(String payNowText3PopUpDisable){
+		this.payNowText3PopUpDisable = payNowText3PopUpDisable;
+	}
+
+	public String getPayNowText3PopUpDisable(){
+		return payNowText3PopUpDisable;
+	}
+
+	public void setPayNowText1Part1(String payNowText1Part1){
+		this.payNowText1Part1 = payNowText1Part1;
+	}
+
+	public String getPayNowText1Part1(){
+		return payNowText1Part1;
+	}
+
+	public void setPayNowText1Part2(String payNowText1Part2){
+		this.payNowText1Part2 = payNowText1Part2;
+	}
+
+	public String getPayNowText1Part2(){
+		return payNowText1Part2;
+	}
+
+	public void setEmiSecondPercentage(String emiSecondPercentage){
+		this.emiSecondPercentage = emiSecondPercentage;
+	}
+
+	public String getEmiSecondPercentage(){
+		return emiSecondPercentage;
+	}
+
+	public void setPayNowText1Part3(String payNowText1Part3){
+		this.payNowText1Part3 = payNowText1Part3;
+	}
+
+	public String getPayNowText1Part3(){
+		return payNowText1Part3;
+	}
+
+	public void setPayNowText1Part4(String payNowText1Part4){
+		this.payNowText1Part4 = payNowText1Part4;
+	}
+
+	public String getPayNowText1Part4(){
+		return payNowText1Part4;
+	}
+
+	public void setEmiOnePercentage(String emiOnePercentage){
+		this.emiOnePercentage = emiOnePercentage;
+	}
+
+	public String getEmiOnePercentage(){
+		return emiOnePercentage;
+	}
+
+	public void setPayNowText1PopUpDisable(String payNowText1PopUpDisable){
+		this.payNowText1PopUpDisable = payNowText1PopUpDisable;
+	}
+
+	public String getPayNowText1PopUpDisable(){
+		return payNowText1PopUpDisable;
+	}
+
+	public void setPayNowPercentage(String payNowPercentage){
+		this.payNowPercentage = payNowPercentage;
+	}
+
+	public String getPayNowPercentage(){
+		return payNowPercentage;
+	}
+
+	public void setPayNowText2PopUpDisable(String payNowText2PopUpDisable){
+		this.payNowText2PopUpDisable = payNowText2PopUpDisable;
+	}
+
+	public String getPayNowText2PopUpDisable(){
+		return payNowText2PopUpDisable;
+	}
+
+	public void setEmiRatesPercentagePopUpDisable(String emiRatesPercentagePopUpDisable){
+		this.emiRatesPercentagePopUpDisable = emiRatesPercentagePopUpDisable;
+	}
+
+	public String getEmiRatesPercentagePopUpDisable(){
+		return emiRatesPercentagePopUpDisable;
+	}
+
+	public void setPayNowText4PopUpDisable(String payNowText4PopUpDisable){
+		this.payNowText4PopUpDisable = payNowText4PopUpDisable;
+	}
+
+	public String getPayNowText4PopUpDisable(){
+		return payNowText4PopUpDisable;
+	}
+
+	public void setPayNowText3(String payNowText3){
+		this.payNowText3 = payNowText3;
+	}
+
+	public String getPayNowText3(){
+		return payNowText3;
+	}
+
+	public void setPayNowImagePopUpDisable(String payNowImagePopUpDisable){
+		this.payNowImagePopUpDisable = payNowImagePopUpDisable;
+	}
+
+	public String getPayNowImagePopUpDisable(){
+		return payNowImagePopUpDisable;
+	}
+
+	public void setPayNowPercentagePopUpDisable(String payNowPercentagePopUpDisable){
+		this.payNowPercentagePopUpDisable = payNowPercentagePopUpDisable;
+	}
+
+	public String getPayNowPercentagePopUpDisable(){
+		return payNowPercentagePopUpDisable;
+	}
+
+	public void setPayNowText2(String payNowText2){
+		this.payNowText2 = payNowText2;
+	}
+
+	public String getPayNowText2(){
+		return payNowText2;
+	}
+}
